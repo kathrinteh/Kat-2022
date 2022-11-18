d80059c530496f32a81f9f5157e52a2a88d507f5
kathrinteh <kathrinkteh@gmail.com>
2022-11-18 09:56:55 -0800

    made buttons work, fixed mobile

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ function setTheme(theme) {
   }
   localStorage.setItem('style', theme);
 }
+
+  /* this is all the overlay hover shit*/
+  $("#pinkieswear").hover(function () {
+    $("#smiley1-overlay").toggle();
+    $("#arrow2-overlay").toggle();
+    $("#pinkieswear-overlay").toggle();
+  });
+
+  $("#smiley1-overlay").hover(function () {
+    $("#smiley1-overlay").show();
+  });
+  $("#smiley1-overlay").mouseout(function () {
+    $("#smiley1-overlay").hide();
+  });
+  $("#arrow2-overlay").hover(function () {
+    $("#arrow2-overlay").show();
+  });
+  $("#arrow2-overlay").mouseout(function () {
+    $("#arrow2-overlay").hide();
+  });
+  $("#pinkieswear-overlay").hover(function () {
+    $("#pinkieswear-overlay").show();
+  });
+  $("#pinkieswear-overlay").mouseout(function () {
+    $("#pinkieswear-overlay").hide();
+  });
